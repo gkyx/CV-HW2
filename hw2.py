d77231fd7f332adebb24ec7583b12ea70024c207
Gökay Gaş <gas15@itu.edu.tr>
2018-11-09 16:16:51 +0300

    Appropriate Menu options are added.

---
+++ hw2.py
@@ -0,0 +1,200 @@
+import cv2
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from PyQt5 import QtGui, QtCore, QtWidgets
+
+############
+# Gokay Gas
+# 150150107
+# 18.10.2018
+############
+
+class Window(QtWidgets.QMainWindow):
+
+	def __init__(self):
+		super(Window, self).__init__()
+		self.setWindowTitle("Filtering & Geometric Transforms")
+		self.setWindowState(QtCore.Qt.WindowMaximized)
+
+		self.Img1 = None
+		self.isInputOpen = False
+
+		mainMenu = self.menuBar()
+
+		fileMenu = mainMenu.addMenu('&File')
+		filtersMenu = mainMenu.addMenu('&Filters')
+		transformsMenu = mainMenu.addMenu('&Geometric Transforms')
+
+		# file menu actions
+
+		openAction = QtWidgets.QAction("Open", self)
+		openAction.triggered.connect(self.open_image)
+
+		saveAction = QtWidgets.QAction("Save", self)
+		saveAction.triggered.connect(self.save_image)
+
+		exitAction = QtWidgets.QAction("Exit", self)
+		exitAction.triggered.connect(QtCore.QCoreApplication.instance().quit)
+
+		fileMenu.addAction(openAction)
+		fileMenu.addAction(saveAction)
+		fileMenu.addAction(exitAction)
+
+		# filter menu actions
+
+		averageFiltersMenu = filtersMenu.addMenu('&Average Filters')
+		gaussianFiltersMenu = filtersMenu.addMenu('&Gaussian Filters')
+		medianFiltersMenu = filtersMenu.addMenu('&Median Filters')
+
+		threeByThreeAction = QtWidgets.QAction("3x3", self)
+		threeByThreeAction.triggered.connect(lambda: self.average_filtering(3))
+		fiveByFiveAction = QtWidgets.QAction("5x5", self)
+		fiveByFiveAction.triggered.connect(lambda: self.average_filtering(5))
+		sevenBySevenAction = QtWidgets.QAction("7x7", self)
+		sevenBySevenAction.triggered.connect(lambda: self.average_filtering(7))
+		nineByNineAction = QtWidgets.QAction("9x9", self)
+		nineByNineAction.triggered.connect(lambda: self.average_filtering(9))
+		elevenByElevenAction = QtWidgets.QAction("11x11", self)
+		elevenByElevenAction.triggered.connect(lambda: self.average_filtering(11))
+		thirteenByThirteenAction = QtWidgets.QAction("13x13", self)
+		thirteenByThirteenAction.triggered.connect(lambda: self.average_filtering(13))
+		fifteenByFifteenAction = QtWidgets.QAction("15x15", self)
+		fifteenByFifteenAction.triggered.connect(lambda: self.average_filtering(15))
+
+		gaussianThreeByThreeAction = QtWidgets.QAction("3x3", self)
+		gaussianThreeByThreeAction.triggered.connect(lambda: self.gaussian_filtering(3))
+		gaussianFiveByFiveAction = QtWidgets.QAction("5x5", self)
+		gaussianFiveByFiveAction.triggered.connect(lambda: self.gaussian_filtering(5))
+		gaussianSevenBySevenAction = QtWidgets.QAction("7x7", self)
+		gaussianSevenBySevenAction.triggered.connect(lambda: self.gaussian_filtering(7))
+		gaussianNineByNineAction = QtWidgets.QAction("9x9", self)
+		gaussianNineByNineAction.triggered.connect(lambda: self.gaussian_filtering(9))
+		gaussianElevenByElevenAction = QtWidgets.QAction("11x11", self)
+		gaussianElevenByElevenAction.triggered.connect(lambda: self.gaussian_filtering(11))
+		gaussianThirteenByThirteenAction = QtWidgets.QAction("13x13", self)
+		gaussianThirteenByThirteenAction.triggered.connect(lambda: self.gaussian_filtering(13))
+		gaussianFifteenByFifteenAction = QtWidgets.QAction("15x15", self)
+		gaussianFifteenByFifteenAction.triggered.connect(lambda: self.gaussian_filtering(15))
+
+		medianThreeByThreeAction = QtWidgets.QAction("3x3", self)
+		medianThreeByThreeAction.triggered.connect(lambda: self.median_filtering(3))
+		medianFiveByFiveAction = QtWidgets.QAction("5x5", self)
+		medianFiveByFiveAction.triggered.connect(lambda: self.median_filtering(5))
+		medianSevenBySevenAction = QtWidgets.QAction("7x7", self)
+		medianSevenBySevenAction.triggered.connect(lambda: self.median_filtering(7))
+		medianNineByNineAction = QtWidgets.QAction("9x9", self)
+		medianNineByNineAction.triggered.connect(lambda: self.median_filtering(9))
+		medianElevenByElevenAction = QtWidgets.QAction("11x11", self)
+		medianElevenByElevenAction.triggered.connect(lambda: self.median_filtering(11))
+		medianThirteenByThirteenAction = QtWidgets.QAction("13x13", self)
+		medianThirteenByThirteenAction.triggered.connect(lambda: self.median_filtering(13))
+		medianFifteenByFifteenAction = QtWidgets.QAction("15x15", self)
+		medianFifteenByFifteenAction.triggered.connect(lambda: self.median_filtering(15))
+
+		averageFiltersMenu.addAction(threeByThreeAction)
+		averageFiltersMenu.addAction(fiveByFiveAction)
+		averageFiltersMenu.addAction(sevenBySevenAction)
+		averageFiltersMenu.addAction(nineByNineAction)
+		averageFiltersMenu.addAction(elevenByElevenAction)
+		averageFiltersMenu.addAction(thirteenByThirteenAction)
+		averageFiltersMenu.addAction(fifteenByFifteenAction)
+
+		gaussianFiltersMenu.addAction(gaussianThreeByThreeAction)
+		gaussianFiltersMenu.addAction(gaussianFiveByFiveAction)
+		gaussianFiltersMenu.addAction(gaussianSevenBySevenAction)
+		gaussianFiltersMenu.addAction(gaussianNineByNineAction)
+		gaussianFiltersMenu.addAction(gaussianElevenByElevenAction)
+		gaussianFiltersMenu.addAction(gaussianThirteenByThirteenAction)
+		gaussianFiltersMenu.addAction(gaussianFifteenByFifteenAction)
+
+		medianFiltersMenu.addAction(medianThreeByThreeAction)
+		medianFiltersMenu.addAction(medianFiveByFiveAction)
+		medianFiltersMenu.addAction(medianSevenBySevenAction)
+		medianFiltersMenu.addAction(medianNineByNineAction)
+		medianFiltersMenu.addAction(medianElevenByElevenAction)
+		medianFiltersMenu.addAction(medianThirteenByThirteenAction)
+		medianFiltersMenu.addAction(medianFifteenByFifteenAction)
+
+		# transform menu actions
+
+		rotateTransformsMenu = transformsMenu.addMenu('&Rotate')
+		scaleTransformsMenu = transformsMenu.addMenu('&Scale')
+		translateTransformsMenu = transformsMenu.addMenu('&Translate')
+
+		rotateRightAction = QtWidgets.QAction("Rotate 10 Degree Right", self)
+		rotateRightAction.triggered.connect(lambda: self.rotation_transform(10))
+		rotateLeftAction = QtWidgets.QAction("Rotate 10 Degree Left", self)
+		rotateLeftAction.triggered.connect(lambda: self.rotation_transform(-10))
+
+		scaleDoubleAction = QtWidgets.QAction("2x", self)
+		scaleDoubleAction.triggered.connect(lambda: self.scale_transform(2))
+		scaleHalfAction = QtWidgets.QAction("1/2x", self)
+		scaleHalfAction.triggered.connect(lambda: self.scale_transform(0.5))
+
+		translateRightAction = QtWidgets.QAction("Right", self)
+		translateRightAction.triggered.connect(lambda: self.translate_transform(1))
+		translateLeftAction = QtWidgets.QAction("Left", self)
+		translateLeftAction.triggered.connect(lambda: self.translate_transform(-1))
+
+		rotateTransformsMenu.addAction(rotateRightAction)
+		rotateTransformsMenu.addAction(rotateLeftAction)
+
+		scaleTransformsMenu.addAction(scaleDoubleAction)
+		scaleTransformsMenu.addAction(scaleHalfAction)
+
+		translateTransformsMenu.addAction(translateRightAction)
+		translateTransformsMenu.addAction(translateLeftAction)
+
+		# central widget for the opened image.
+		self.centralwidget = QtWidgets.QWidget(self)
+		self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+		self.horizontalLayout.setContentsMargins(100, 10, 100, 10)
+		self.horizontalLayout.setSpacing(100)
+		self.setCentralWidget(self.centralwidget)
+		
+		self.show()
+
+
+	def open_image(self):
+		# one function for both of the input and target images
+		if not self.isInputOpen:
+			# Image
+
+			pix = QtGui.QPixmap('color1.png')
+			label = QtWidgets.QLabel(self.centralwidget)
+			label.setPixmap(pix)
+			label.setAlignment(QtCore.Qt.AlignCenter)
+			label.setStyleSheet("border:0px")
+			self.horizontalLayout.addWidget(label)
+
+			self.isInputOpen = True
+
+	def save_image(self):
+		raise NotImplementedError
+
+	def average_filtering(self, size):
+		raise NotImplementedError
+
+	def gaussian_filtering(self, size):
+		raise NotImplementedError
+
+	def median_filtering(self, size):
+		raise NotImplementedError
+
+	def rotation_transform(self, size):
+		raise NotImplementedError
+
+	def scale_transform(self, size):
+		raise NotImplementedError
+
+	def translate_transform(self, size):
+		raise NotImplementedError
+
+
+def main():
+	app = QtWidgets.QApplication(sys.argv)
+	GUI = Window()
+	sys.exit(app.exec_())
+
+main()
